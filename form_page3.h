c9b045da2359bf764cebde080a7d97ab187bb393
liubai <704583217@qq.com>
2025-09-18 19:41:38 +0800

    简化form_page3类的实现，移除不必要的成员函数和变量

--- form_page3.h
+++ form_page3.h
@@ -2,8 +2,6 @@
 #define FORM_PAGE3_H
 
 #include <QWidget>
-#include<QStandardItemModel>
-
 
 namespace Ui {
 class Form_page3;
@@ -16,19 +14,9 @@ class Form_page3 : public QWidget
 public:
     explicit Form_page3(QWidget *parent = 0);
     ~Form_page3();
-    void initchkresult(int Type,QString filepath);
-
-private slots:
-    void on_pushButton_gen_all_clicked();
 
 private:
     Ui::Form_page3 *ui;
-    QStandardItemModel*  mTableViewModel;
-    void paintTable_randomtest();
-    void paintTable_streamcipher_all();
-    void paintTable_blockcipher_all();
-signals:
-    swichrightpage(int Type);
 };
 
 #endif // FORM_PAGE3_H
